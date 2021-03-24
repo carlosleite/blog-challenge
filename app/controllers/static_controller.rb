580c675d41000547bfb32ac4297ad3fd5b76125c
Carlos Leite <carlosleitebos@gmail.com>
2021-03-24 00:44:58 -0300

    Added rubocop, es lint and set react initial app

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Controller to serve react app
+class StaticController < ApplicationController
+  def index; end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# main application helper
 module ApplicationHelper
 end
